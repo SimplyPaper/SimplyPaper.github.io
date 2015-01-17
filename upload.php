db6ed31c2005318f5b903c41b35bfa916be6d5ff
Advith Chelikani <advith.chelikani@gmail.com>
2015-01-17 04:17:08 -0500

    update

--- upload.php
+++ upload.php
@@ -1,4 +1,8 @@
 <?php
+header('Location: http://localhost:8888/home.html');
+$file = 'log.txt';
+file_put_contents($file, "hello");
+$current = file_get_contents($file);
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -7,38 +11,45 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        $current .= "File is an image - " . $check["mime"] . ".";
+        file_put_contents($file, $contents);
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        $current .= "File is not an image.";
+        file_put_contents($file, $contents);
         $uploadOk = 0;
     }
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    $current .= "Sorry, file already exists.";
+    file_put_contents($file, $contents);
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+    $current .= "Sorry, your file is too large.";
+    file_put_contents($file, $contents);
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $current .= "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    file_put_contents($file, $contents);
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    $current .= "Sorry, your file was not uploaded.";
+    file_put_contents($file, $contents);
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        $current .= "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        $current .= "Sorry, there was an error uploading your file.";
+        file_put_contents($file, $contents);
     }
 }
 ?>
